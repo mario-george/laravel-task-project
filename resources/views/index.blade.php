de477bb6cf2b2ac878c5ad02359e7e07a43e35a6
mario-george <vortexspades@gmail.com>
2024-01-19 01:47:29 +0200

    Add index.blade.php template file

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+<h1>
+    blade is a template engine for php included with laravel
+    adds directives starts with @ symbol
+</h1>
+<p>
+@isset($name)
+  <div>The name is: {{ $name }}</div>
+@endisset
+</p>
