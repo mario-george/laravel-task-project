be978b6f38237712effc585c0afb90ce749318a1
mario-george <vortexspades@gmail.com>
2024-01-19 11:50:59 +0200

    Add tasksIndex and tasksShow views

---
+++ resources/views/tasksIndex.blade.php
@@ -0,0 +1,20 @@
+@foreach($tasks as $task)
+<p>
+
+    {{$task->title}}
+</p>
+
+@endforeach
+@forelse($tasks as $task)
+<a href="/tasks/{{$task->id}}">{{$task->title}}</a><br>
+@empty
+<h1>There is no tasks</h1>
+
+@endforelse
+<h1>route() function generates a URL for the named route</h1>
+
+@forelse($tasks as $task)
+<a href="{{route('tasks.show',['id'=>$task->id])}}">{{$task->title}}</a>
+<br>
+@empty
+@endforelse
---
+++ resources/views/tasksShow.blade.php
@@ -0,0 +1,2 @@
+<h1>Task {{$task->title}}</h1>
+<h1>Task {{$task->id}}</h1>
