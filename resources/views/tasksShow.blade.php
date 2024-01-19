125469c2fd2b93fc1267b52ab530127402290078
mario-george <vortexspades@gmail.com>
2024-01-19 16:50:27 +0200

    Refactor tasksShow.blade.php to include layout and display task details

--- resources/views/tasksShow.blade.php
+++ resources/views/tasksShow.blade.php
@@ -1,2 +1,15 @@
-<h1>Task {{$task->title}}</h1>
-<h1>Task {{$task->id}}</h1>
+
+@extends('layouts.layout')
+
+@section('title', $task->title)
+<!-- no need for endsection if you are passing a string on the second arg of section -->
+@section('content')
+  <p>{{ $task->description }}</p>
+
+  @if ($task->long_description)
+    <p>{{ $task->long_description }}</p>
+  @endif
+
+  <p>{{ $task->created_at }}</p>
+  <p>{{ $task->updated_at }}</p>
+@endsection
