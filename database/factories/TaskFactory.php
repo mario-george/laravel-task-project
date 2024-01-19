87220ffe0daaf24b611746641367e4675be460f6
mario-george <vortexspades@gmail.com>
2024-01-19 20:51:59 +0200

    Update TaskFactory.php with Faker data generation

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -17,7 +17,16 @@ class TaskFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            // sentence() or property / () can be a method if you want to configure the value or property
+            'title'=>fake()->sentence,
+            'description'=>fake()->paragraph,
+            'long_description'=>fake()->paragraph(9,true),
+            'completed' => fake()->boolean
+            /* 
+The paragraph method in Faker generates a paragraph of text, containing a given amount of sentences. The second parameter is a boolean that determines the format of the output:
+If the second parameter is true, the method will return a string.
+If the second parameter is false or not provided, the method will return an array.
+ */
         ];
     }
 }
