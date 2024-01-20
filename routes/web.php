c77fc1466cdbebd09b769cbddb063eba9746a368
mario-george <vortexspades@gmail.com>
2024-01-20 23:27:52 +0200

    Add edit and update routes for tasks

--- routes/web.php
+++ routes/web.php
@@ -81,6 +81,25 @@ Route::get('/tasks/{id}', function ($id){
   
   return view('tasksShow',['task'=>Task::findOrFail($id)]);
 })->name('tasks.show'); 
+Route::get('/tasks/{id}/edit', function ($id){
+  
+  return view('edit',['task'=>Task::findOrFail($id)]);
+})->name('tasks.edit');
+Route::put('/tasks/{id}/edit', function ($id,Request $request) {
+
+  $data=$request->validate([
+    'title'=>'required|max:255',
+    'description'=>'required',
+    'long_description'=>'required',
+  ]);
+  $task=Task::findOrFail($id);
+  $task->title=$data['title'];
+  $task->description=$data['description'];
+  $task->long_description=$data['long_description'];
+  
+  $task->save();
+  return redirect()->route('tasks.show',['id'=>$task['id']])->with('success','Task edited successfully');
+  })->name('tasks.update');
 Route::post('/tasks', function (Request $request) {
 
 $data=$request->validate([
@@ -94,7 +113,7 @@ $task->description=$data['description'];
 $task->long_description=$data['long_description'];
 
 $task->save();
-return redirect()->route('tasks.show',['id'=>$task['id']]);
+return redirect()->route('tasks.show',['id'=>$task['id']])->with('success','Task created successfully');
 })->name('tasks.store');
 
 /* 
